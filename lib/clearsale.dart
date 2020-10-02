83e17c41b3dadc029170cd2d14a067f5917be7de
David Araujo <david@flutterando.com.br>
2020-10-02 07:39:47 -0300

    Exports do package

--- lib/clearsale.dart
+++ lib/clearsale.dart
@@ -1,1 +1,17 @@
 library clearsale;
+
+export 'src/presenter/clearsale.dart';
+export 'src/presenter/clearsale.dart';
+
+export 'src/domain/models/analysis_request_model.dart';
+export 'src/domain/models/analisys_response_model.dart';
+export 'src/domain/models/token_model.dart';
+export 'src/domain/models/chargeback_marking_response_model.dart';
+export 'src/domain/models/response_model.dart';
+export 'src/domain/models/message_model.dart';
+export 'src/domain/models/order_model.dart';
+
+export 'src/domain/errors/failure.dart';
+export 'src/domain/errors/repository.dart';
+export 'src/domain/errors/usecases.dart';
+export 'src/domain/errors/datasource.dart';
