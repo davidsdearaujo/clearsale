6175908d1fd17080b12903f660f679b61266201d
David Araujo <david@flutterando.com.br>
2020-11-23 16:16:01 -0300

    Updated PhoneTypeEnum

--- lib/src/domain/enums/phone_type_enum.dart
+++ lib/src/domain/enums/phone_type_enum.dart
@@ -1,17 +1,37 @@
 ///### Objeto phones
 ///**Importante**: É obrigatório o envio de pelo menos 1 telefone.
 enum PhoneTypeEnum {
-  ///Pessoa Física
-  physicalPhone,
+  ///Indefinido
+  undefined,
 
-  ///Pessoa Jurídica
-  legalEntity,
+  ///Residencial
+  residential,
+
+  ///Comercial
+  commercial,
+
+  ///Recados
+  scraps,
+
+  ///Cobrança
+  collection,
+
+  ///Temporário
+  temporary,
+
+  ///Celular
+  cell_phone,
 }
 
 extension PhoneTypeEnumExtension on PhoneTypeEnum {
   static Map<PhoneTypeEnum, int> get _values => const {
-        PhoneTypeEnum.physicalPhone: 1,
-        PhoneTypeEnum.legalEntity: 2,
+        PhoneTypeEnum.undefined: 0,
+        PhoneTypeEnum.residential: 1,
+        PhoneTypeEnum.commercial: 2,
+        PhoneTypeEnum.scraps: 3,
+        PhoneTypeEnum.collection: 4,
+        PhoneTypeEnum.temporary: 5,
+        PhoneTypeEnum.cell_phone: 6,
       };
   int toMap() => _values[this];
   static PhoneTypeEnum fromMap(int val) {
