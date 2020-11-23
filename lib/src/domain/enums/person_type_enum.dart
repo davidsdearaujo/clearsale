5a94c784fd7c13e53643c4d705f8192eb2a6fc17
David Araujo <david@flutterando.com.br>
2020-11-23 16:08:00 -0300

    Updated person types

--- lib/src/domain/enums/person_type_enum.dart
+++ lib/src/domain/enums/person_type_enum.dart
@@ -2,16 +2,16 @@
 ///https://api.clearsale.com.br/docs/total-totalGarantido-application#person-type
 enum PersonTypeEnum {
   ///Pessoa Física
-  individual,
+  individual_person,
 
   ///Pessoa Jurídica
-  legal,
+  corporate_entity,
 }
 
 extension PersonTypeEnumExtension on PersonTypeEnum {
   static Map<PersonTypeEnum, int> get _values => const {
-        PersonTypeEnum.individual: 1,
-        PersonTypeEnum.legal: 2,
+        PersonTypeEnum.individual_person: 1,
+        PersonTypeEnum.corporate_entity: 2,
       };
   int toMap() => _values[this];
   static PersonTypeEnum fromMap(int val) {
